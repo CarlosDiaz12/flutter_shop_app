51038b955e9b80b21e8be18dbaa7a7da8555c6f8
Carlos Diaz <carlos.cedp@gmail.com>
2021-06-26 18:43:41 -0400

    🎉 initial config

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Shop App'),
+      ),
+      body: SafeArea(
+          child: Center(
+        child: Text('Shop App'),
+      )),
+    );
+  }
+}
