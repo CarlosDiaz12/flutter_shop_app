ac2bc2aee323b8662181f95d7b450b7d79f65e1f
Carlos Diaz <carlos.cedp@gmail.com>
2021-06-27 14:16:52 -0400

    ➕ add provider dependency

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Shop App',
       theme: AppTheme.theme,
       home: ProductsOverViewPage(),
