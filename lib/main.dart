768be6f1354a7e06688dcad12818b9f793474baf
Carlos Diaz <carlos.cedp@gmail.com>
2021-06-27 16:52:29 -0400

    ♻️ refactor main change norifier

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,8 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: ProductsProvider(),
+    return ChangeNotifierProvider(
+      create: (_) => ProductsProvider(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Shop App',
