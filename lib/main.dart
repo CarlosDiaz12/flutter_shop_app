ac000453505f4af2c5b2b77144f200e8de7b30b3
Carlos Diaz <carlos.cedp@gmail.com>
2021-06-27 14:33:45 -0400

    🚧 register products provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_shop_app/config/app_theme.dart';
 import 'package:flutter_shop_app/pages/product_detail_page.dart';
 import 'package:flutter_shop_app/pages/products_overview_page.dart';
+import 'package:flutter_shop_app/providers/products_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(App());
@@ -10,13 +12,16 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shop App',
-      theme: AppTheme.theme,
-      home: ProductsOverViewPage(),
-      initialRoute: ProductsOverViewPage.routeName,
-      routes: {ProductDetailPage.routeName: (ctx) => ProductDetailPage()},
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Shop App',
+        theme: AppTheme.theme,
+        home: ProductsOverViewPage(),
+        initialRoute: ProductsOverViewPage.routeName,
+        routes: {ProductDetailPage.routeName: (ctx) => ProductDetailPage()},
+      ),
     );
   }
 }
